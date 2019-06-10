a183531683c21030e5c0f14e38e7feae79cf6daf
Tomasz Sęk <tomasz.sek.88@gmail.com>
2019-06-10 17:12:41 +0200

    Add v1alpha2 Jenkins schema

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -1,6 +1,9 @@
 package apis
 
 import (
+	"github.com/jenkinsci/kubernetes-operator/pkg/apis/jenkins/v1alpha1"
+	"github.com/jenkinsci/kubernetes-operator/pkg/apis/jenkins/v1alpha2"
+
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -10,4 +13,11 @@ var AddToSchemes runtime.SchemeBuilder
 // AddToScheme adds all Resources to the Scheme
 func AddToScheme(s *runtime.Scheme) error {
 	return AddToSchemes.AddToScheme(s)
+}
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha2.SchemeBuilder.AddToScheme)
 }
