bd6012dfb1cede1af1e2d384d8b1013dd7833c8d
David Ashford <david.ashford@distilled.ie>
2019-08-07 16:11:13 +0100

    add grep command to only filter backups

--- backup/pvc/bin/run.sh
+++ backup/pvc/bin/run.sh
@@ -10,6 +10,6 @@ do
     sleep 10
     if [[ ! -z "${BACKUP_COUNT}" ]]; then
         echo "Trimming to only ${BACKUP_COUNT} recent backups in preparation for new backup"
-        ls ${BACKUP_DIR} | sort -gr | tail -n +$((BACKUP_COUNT +1)) | xargs -I '{}' rm ${BACKUP_DIR}/'{}'
+        ls ${BACKUP_DIR} | sort -gr | grep 'tar.gz' | tail -n +$((BACKUP_COUNT +1)) | xargs -I '{}' rm ${BACKUP_DIR}/'{}'
     fi
 done
