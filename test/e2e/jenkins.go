c90969256eab00da0030fc92992a19cae99b871f
Oleksandr Kovalchuk <anxolerd@outlook.com>
2020-04-11 15:06:04 +0300

    Use existing version of envoyproxy image

--- test/e2e/jenkins.go
+++ test/e2e/jenkins.go
@@ -110,7 +110,7 @@ func createJenkinsCR(t *testing.T, name, namespace string, seedJob *[]v1alpha2.S
 					},
 					{
 						Name:  "envoyproxy",
-						Image: "envoyproxy/envoy-alpine",
+						Image: "envoyproxy/envoy-alpine:v1.14.1",
 					},
 				},
 				Plugins: []v1alpha2.Plugin{
