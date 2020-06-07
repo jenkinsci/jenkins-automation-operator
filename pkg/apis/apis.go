445cf2eb8c498b015eab2c867a52b00420f8037b
Tomasz Sęk <tomasz.sek.88@gmail.com>
2020-06-07 19:17:39 +0200

    Remove unwanted schemes
    https://github.com/operator-framework/operator-sdk/issues/2577

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -2,8 +2,6 @@ package apis
 
 import (
 	"github.com/jenkinsci/kubernetes-operator/pkg/apis/jenkins/v1alpha2"
-	routev1 "github.com/openshift/api/route/v1"
-	appsv1 "k8s.io/api/apps/v1"
 
 	"k8s.io/apimachinery/pkg/runtime"
 )
@@ -19,6 +17,4 @@ func AddToScheme(s *runtime.Scheme) error {
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes, v1alpha2.SchemeBuilder.AddToScheme)
-	AddToSchemes = append(AddToSchemes, routev1.AddToScheme)
-	AddToSchemes = append(AddToSchemes, appsv1.AddToScheme)
 }
