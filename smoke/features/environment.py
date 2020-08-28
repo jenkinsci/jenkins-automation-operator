97dd8a4922473e891f688e5eb2377f6e4fb9422c
jsingh <jitsingh@redhat.com>
2020-08-28 12:18:59 +0530

    refactoring env deploy project for smoketest

--- smoke/features/environment.py
+++ smoke/features/environment.py
@@ -14,18 +14,23 @@ from pyfiglet import Figlet
 from pyshould import should
 from clint.textui import puts,colored, indent
 
-'''
-before_scenario(context, scenario), after_scenario(context, scenario)
-    These run before and after each scenario is run.
-    The scenario passed in is an instance of Scenario.
-'''
+
 f = Figlet(font='starwars', width=100)
 
 def before_feature(_context,_feature):
+    '''
+    before_feature(context, feature), after_feature(context, feature)
+    These run before and after each feature is run.
+    '''
     print(colored.red(f.renderText('Jenkins Operator')))
     print(colored.yellow("checking cluster environment for {} scenario".format(_feature)))
 
 def before_scenario(_context, _scenario):
+    '''
+    before_scenario(context, scenario), after_scenario(context, scenario)
+    These run before and after each scenario is run.
+    The scenario passed in is an instance of Scenario.
+'''
     print(colored.yellow("Checking cluster environment for {} scenario".format(_scenario)))
     code, output = subprocess.getstatusoutput('oc get project default')
     print(colored.yellow("[CODE] {}".format(code)))
